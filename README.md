4a6852200307ae64faff5264609bfc5016a2a80c
Judicael MARION <judi.marion1812@gmail.com>
2025-06-28 13:03:45 +0200

    creation script init + update readme

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # TDLExercise
-
-
 Exercice Technique dans le cadre de ma candidature chez NoetIA
+
+## Use
+
+### 0- Initialiser le projet et récupérer les dépendances 
+``` 
+chmod u+x init.sh
+./init.sh
+```
+
+### 1- Lancer le serveur : 
+```
+fastapi run src/server.py
+```
+
+### 2- Lancer le frontend:
+
+A venir
+
+
+## Issues:
+```
+ERROR: python is not installed on this machine
+```
+
+fix : installer python ou python3
---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if command -V python python3
+    then
+        #cleanup si déjà installé
+        if ls -a | grep ".venv"
+        then 
+            rm -rf .venv
+        fi
+        #creation venv + install requirements
+        python -m venv .venv
+        source .venv/bin/activate
+        pip install -r requirements.txt
+        echo "All done"
+        exit 0
+    else
+        echo "ERROR: python is not installed on this machine"
+        exit 1
+    fi
